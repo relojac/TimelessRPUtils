e77963bef2a2d9e0a1908d98b6afc979ab9af2dd
relojac <121554584+relojac@users.noreply.github.com>
2025-06-25 19:17:49 +0800

    Update EXECUTEME.lua

--- EXECUTEME.lua
+++ EXECUTEME.lua
@@ -1,6 +1,51 @@
 local genv = (getgenv and getgenv()) or shared
 genv.Settings = {
-	[""] = true
+	["Buttons"] = {
+		["Drop"] = { -- Lets you drop an item if it is droppable.
+			["Enabled"] = true,
+			["Position"] = { -3, 1 },
+
+			["DroppableRequired"] = true
+		},
+		
+		["Faint"] = { -- Disables your own Humanoid physics when on.
+			["Enabled"] = true,
+			["Position"] = { -2, 0 }
+		},
+		
+		["NightVision"] = { -- Lets you see in the dark and highlights Player Characters.
+			["Enabled"] = true,
+			["Position"] = { 0, 2 }
+		}, 
+		
+		["Reset"] = { -- Kills your character on press.
+			["Enabled"] = true,
+			["Position"] = { -2, 1 }
+		},
+		
+		["Sit"] = {
+			["Enabled"] = true,
+			["Position"] = { -1, 0 }
+		},
+		
+		["Sprint"] = {
+			["Enabled"] = true,
+			["Position"] = { -1, 1 },
+
+			["Toggled"] = false,
+			["IsTweened"] = true
+		},
+	},
+
+	["Effects"] = {
+		["NightVision"] = {
+			["ColorCorrection"] = true,
+			["PlayerHighlights"] = true,
+			["Vignette"] = true,
+
+			["Psychopathic"] = true
+		}
+	}
 }
 
 loadstring(game:HttpGet("https://github.com/relojac/TimelessRPUtils/raw/refs/heads/main/Core/Main.lua"))()
