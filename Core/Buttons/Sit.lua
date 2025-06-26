753c1debdfe5d6295fb1ce7e49ddb41ceedbd338
relojac <121554584+relojac@users.noreply.github.com>
2025-06-26 19:04:59 +0800

    Update Sit.lua

--- Core/Buttons/Sit.lua
+++ Core/Buttons/Sit.lua
@@ -1,1 +1,39 @@
-a
+local genv = (getgenv and getgenv()) or shared
+
+local Module = genv.Module
+
+local Button = Module.Button
+local Positioner = Module.Positioner
+
+
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+local Player = Players.LocalPlayer
+local Character = Player.Character or Player.CharacterAdded:Wait()
+local Humanoid = Character:WaitForChild("Humanoid")
+
+Player.CharacterAdded:Connect(function(new)
+	Character = new
+	Humanoid = Character:WaitForChild("Humanoid")
+end)
+
+local button = Button.new("SitButton", true)
+	button.PressedImage = nil
+	button.Position = Positioner.do(-1, 0)
+
+button.MouseButton1Click:Connect(function()
+	if not Humanoid.Sit then
+		Humanoid.Sit = true
+	else
+		Humanoid.Sit = false
+	end
+end)
+
+RunService.Stepped:Connect(function()
+	if not Humanoid.Sit then
+		button.Image = getsynasset("SitButton_off.png")
+	else
+		button.Image = getsynasset("SitButton_on.png")
+	end
+end)
