59f7aa2438b92f203772f72654659602a9c8bc7a
relojac <121554584+relojac@users.noreply.github.com>
2025-06-26 18:44:13 +0800

    Update Button.lua

--- Core/Module/Button.lua
+++ Core/Module/Button.lua
@@ -16,25 +16,36 @@ local PlayerGui = Player:WaitForChild("PlayerGui")
 local JB = Values.Gui.JumpButton
 local Reference = JB.Reference
 
-local function createTextBtn()
-	
-end
 
-local function createImgBtn()
-	local b = Instance.new("ImageButton", Reference)
-		b.Image = ""
-		b.PressedImage = ""
-end
-
-
-
-function Button.new(toggleable, active, imgBtn)
-	local button
-	if imgBtn then
-		button = createImgBtn()
-	else
-		button = createTextButton()
+function Button.new(name, active)
+	if typeof(name) ~= "string" then
+		warn("name arg is not a string")
+	elseif typeof(active) ~= "bool" then
+		warn("active arg is not a bool")
 	end
-
 	
+	local b = Instance.new("ImageButton", Reference)
+		b.Name = name or "Button"
+		b.Image = getsynasset("Button_off.png")
+		b.PressedImage = getsynasset("Button_on.png")
+		b.Active = active or true
+		b.BackgroundTransparency = 1
+
+	return b
+end
+
+function Button.del(name)
+	if typeof(name) ~= "string" then
+		warn("name arg is not a string")
+		return
+	end
+	
+	local b = Reference:FindFirstChild(name)
+
+	if b then
+		b:Destroy()
+	else
+		warn(name, "is not a valid Button object")
+	end
+	return
 end
