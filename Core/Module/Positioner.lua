99d70f93abd4ce8fecbb0989c90f4cf8758d3d07
relojac <121554584+relojac@users.noreply.github.com>
2025-06-26 11:31:46 +0800

    Update Positioner.lua

--- Core/Module/Positioner.lua
+++ Core/Module/Positioner.lua
@@ -1,10 +1,12 @@
 local genv = (getgenv and getgenv()) or shared
 local Settings = genv.Settings
 
+local ButtonSpacing = Settings.ButtonSpacing or 0.1
+
 genv.Module = genv.Module or {}
 local Module = genv.Module
 
-local butSp = 1.1
+local butSp = 1 + ButtonSpacing
 
 function Module.Positioner(xM, yM, Button)
 	local X = xM * 1.1
