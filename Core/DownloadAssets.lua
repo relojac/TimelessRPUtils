4d7631900c02e1e5327bdfc219d94143a874b28a
relojac <121554584+relojac@users.noreply.github.com>
2025-06-26 18:01:52 +0800

    Create DownloadAssets.lua

---
+++ Core/DownloadAssets.lua
@@ -0,0 +1,31 @@
+local genv = (getgenv and getgenv()) or shared
+
+local Module = genv.Module
+local Asset = Module.Asset
+
+local getsynasset = getsynasset or getcustomasset or function() end
+
+local buttons = {
+	"Button_off.png",
+	"Button_on.png", 
+
+	"DropButton_off.png",
+	"DropButton_on.png", 
+
+	"JumpButton_off.png",
+	"JumpButton_on.png", 
+
+	"NVButton_off.png",
+	"NVButton_on.png", 
+
+	"ResetButton_off.png",
+	"ResetButton_on.png", 
+
+	"SitButton_off.png",
+	"SitButton_on.png"
+}
+
+for _, v in ipairs(buttons) do
+	Asset.write("https://github.com/relojac/TimelessRPUtils/raw/refs/heads/main/Assets/Buttons/"..v, v)
+	print(getsynasset(v))
+end
