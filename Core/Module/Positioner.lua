82ce92c474effb0657fecaef8057b6ed28843d0f
relojac <121554584+relojac@users.noreply.github.com>
2025-06-26 18:50:10 +0800

    Update Positioner.lua

--- Core/Module/Positioner.lua
+++ Core/Module/Positioner.lua
@@ -12,8 +12,12 @@ local Positioner = genv.Positioner
 local butSp = 1 + ButtonSpacing
 
 function Positioner.do(xM, yM)
-	local X = (xM or 1) * butSp
+	local X = (xM or -1) * butSp
 	local Y = (yM or 0) * (butSp*-1)
 
 	return UDim2.fromScale(X, Y)
+end
+
+function Positioner.reset(button)
+	button.Position = UDim2.fromScale(0, 0)
 end
