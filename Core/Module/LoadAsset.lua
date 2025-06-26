f7f13b76664b91350d237a3a8bba9491b03a733b
relojac <121554584+relojac@users.noreply.github.com>
2025-06-26 12:02:49 +0800

    Update LoadAsset.lua

--- Core/Module/LoadAsset.lua
+++ Core/Module/LoadAsset.lua
@@ -8,12 +8,25 @@ local request = syn and syn.request or http and http.request or request or funct
 local isfile = isfile or readfile and function(filename) local succ,a = pcall(function() local b = readfile(filename) end) return succ end or function() end
 
 function Module.loadVideo(url, videoName)
-	if videoName 
+	if not videoName:match(".webm") then
+		videoName = videoName..".webm" -- VideoFrames only support webm
+	end
 	if getsynasset and request and writefile and isfile then
 		if not isfile(videoName) then
 			local Response, TempFile = request({Url = url, Method = 'GET'})
 			if Response.StatusCode == 200 then
 				writefile(videoName, Response.Body)
+			end
+		end
+	end
+end
+
+function Module.loadImage(url, imageName)
+	if getsynasset and request and writefile and isfile then
+		if not isfile(imageName) then
+			local Response, TempFile = request({Url = url, Method = 'GET'})
+			if Response.StatusCode == 200 then
+				writefile(imageName, Response.Body)
 			end
 		end
 	end
