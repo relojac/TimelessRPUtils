bcc14649f2697a6f675aa2b3874d95dfab27313d
relojac <121554584+relojac@users.noreply.github.com>
2025-06-26 21:13:36 +0800

    Create AssetTable.lua

---
+++ AssetTable.lua
@@ -0,0 +1,38 @@
+local genv = (getgenv and getgenv()) or shared
+
+genv.AllAssets = genv.AllAssets or {}
+local AllAssets = genv.AllAssets
+
+AllAssets.Buttons = {
+	"Button_off.png",
+	"Button_on.png", 
+
+	"DropButton_off.png",
+	"DropButton_on.png", 
+
+	"JumpButton_off.png",
+	"JumpButton_on.png", 
+
+	"NVButton_off.png",
+	"NVButton_on.png", 
+
+	"ResetButton_off.png",
+	"ResetButton_on.png", 
+
+	"SitButton_off.png",
+	"SitButton_on.png"
+}
+
+AllAssets.AutoLoadedEffects = {
+	"vignette.png"
+}
+
+AllAssets.PsychopathicFlash = {
+	"Charts.png",
+	"Letter.png",
+	"Ogscule.png",
+	"Omniscience.png",
+	"Positioner.png",
+	"Sky.png",
+	"Verboten.png"
+}
