463c6c7b4327ac9a5f846c6e8b1cfb44ff46107d
relojac <121554584+relojac@users.noreply.github.com>
2025-06-27 21:02:00 +0800

    Update Sprint.lua

--- Core/Buttons/Sprint.lua
+++ Core/Buttons/Sprint.lua
@@ -1,16 +1,27 @@
+--[[
+	Hii!! It's me again! You remember me, right? Ms. Solyn A. Placeholder? From Main.lua?
+	No? Awhh... it's fine, I'm used to being forgotten, don't worry.
+
+	Anyway, what do we have here...
+	Oh, the Sprint button! I like this one!
+
+	It's the second most useful button.
+	I'll explain along the way~!!⭐
+]]
+
 local genv = (getgenv and getgenv()) or shared
 
-local Settings = genv.Settings
-local Module = genv.Module
+local Settings = genv.Settings -- This should get your Settings! If you executed it with them.
+local Module = genv.Module -- Module will just keep appearing! Of course, these aren't actually ModuleScripts, but they're genv emulations of them.
 
 local Button = Module.Button
 local Positioner = Module.Positioner
 local Asset = Module.Asset
 
-local Toggled = Settings.Buttons.Sprint.Toggled or false
-local IsTweened = Settings.Buttons.Sprint.IsTweened or true
+local Toggled = Settings.Buttons.Sprint.Toggled or false -- This will determine whether or not the button will be Held or Toggled. Default is Held/false, if you didn't know already!
+local IsTweened = Settings.Buttons.Sprint.IsTweened or true -- Of course, this is mostly visual. But it looks smooth!
 
-local m = Settings.Buttons.Sprint.Multipliers or {}
+local m = Settings.Buttons.Sprint.Multipliers or {} -- Yes, your Base WalkSpeed/Field of View will be multiplied by these values.
 local WSm = m.WalkSpeed or 1.5
 local FOVm = m.FieldOfView or 1.15
 
@@ -20,11 +31,11 @@ local TweenService = game:GetService("TweenService")
 
 local info = TweenInfo.new(
 	0.5,
-	Enum.EasingStyle.Exponential,
-	Enum.EasingDirection.Out
+	Enum.EasingStyle.Exponential, -- If you're familiar with mathematics, you would naturally understand this.
+	Enum.EasingDirection.Out -- It's like.. Think of an Expo In. It gradually gets faster until you reach the end. Now this one is backwards! Instead of getting faster, it starts really fast, then gets slower and slower. Sorry, I'm talking too much. Sorry...
 )
 
-local Player = Players.LocalPlayer
+local Player = Players.LocalPlayer -- Hi You!! („>ᵕ🌟„)
 local Character = Player.Character or Player.CharacterAdded:Wait()
 local Humanoid = Character:WaitForChild("Humanoid")
 local Camera = workspace.CurrentCamera
@@ -32,7 +43,7 @@ local Camera = workspace.CurrentCamera
 local BaseWS = Humanoid.WalkSpeed or 16
 local BaseFOV = Camera.FieldOfView or 70
 
-local sprinting = Instance.new("BoolValue", Character)
+local sprinting = Instance.new("BoolValue", Character) -- This'll indicate whether you're sprinting or not!
 	sprinting.Name = "Sprinting"
 	sprinting.Value = false
 
@@ -102,7 +113,7 @@ else
 		sprintStart(Humanoid, Camera)
 	end)
 
-	button.MouseButton1Up:Connect(function()
+	button.MouseButton1Up:Connect(function() -- MouseButton1Up is kind of... er, terrible, sometimes. Unfortunately, Ms. relojac can't really do much about it.
 		sprinting.Value = false
 		sprintStop(Humanoid, Camera)
 	end) 
@@ -122,6 +133,6 @@ RunService.RenderStepped:Connect(function()
 	else
 		button.Visible = false
 		sprinting.Value = false
-		sprintStop(Humanoid, Camera)
+		sprintStop(Humanoid, Camera) -- Because Roblox is wack, if a Button becomes invisible before you can MouseButton1Up it, it'll never fire until you do it once it's visible again. Of course, we'll have to work with just calling sprintStop again... (⁠╥⁠﹏⁠💫⁠)
 	end
 end) 
