d98c28103d8561800657281e0ba0e047c530bd6a
relojac <121554584+relojac@users.noreply.github.com>
2025-06-27 09:00:12 +0800

    Update Asset.lua

--- Core/Module/Asset.lua
+++ Core/Module/Asset.lua
@@ -11,60 +11,63 @@ local request = syn and syn.request or http and http.request or request or funct
 local isfile = isfile or readfile and function(filename) local succ,a = pcall(function() local b = readfile(filename) end) return succ end or function() end
 local writefile = writefile or function() end
 local delfile = delfile or function() end
+local makefolder = makefolder or function() end
+local isfolder = isfolder or function() end
+local delfolder = delfolder or function() end
 
-local works = getsynasset and request and writefile and isfile and delfile
+local works = getsynasset and request and writefile and isfile and defile and makefolder and isfolder and delfolder
+
+if works then
+	makefolder("Timeless")
+end
 	
 function Asset.video(url, videoName)
-	if not videoName:match(".webm") then
-		videoName = videoName..".webm"
-	end
-	
 	if works then
-		if not isfile(videoName) then
+		if not isfile("Timeless/"..videoName) then
 			local Response, TempFile = request({Url = url, Method = 'GET'})
 			if Response.StatusCode == 200 then
-				writefile(videoName, Response.Body)
+				writefile("Timeless/"..videoName, Response.Body)
 			end
 		end
 
-		return getsynasset(videoName) or nil
+		return getsynasset("Timeless/"..videoName) or nil
 	end
 end
 
 function Asset.image(url, imageName)
 	if works then
-		if not isfile(imageName) then
+		if not isfile("Timeless/"..imageName) then
 			local Response, TempFile = request({Url = url, Method = 'GET'})
 			if Response.StatusCode == 200 then
-				writefile(imageName, Response.Body)
+				writefile("Timeless"..imageName, Response.Body)
 			end
 		end
 
-		return getsynasset(imageName) or "rbxasset://textures/ui/GuiImagePlaceholder.png"
+		return getsynasset("Timeless/"..imageName) or "rbxasset://textures/ui/GuiImagePlaceholder.png"
 	end
 end
 
 function Asset.audio(url, audioName)
 	if works then
-		if not isfile(audioName) then
+		if not isfile("Timeless/"..audioName) then
 			local Response, TempFile = request({Url = url, Method = 'GET'})
 			if Response.StatusCode == 200 then
-				writefile(audioName, Response.Body)
+				writefile("Timeless/"..audioName, Response.Body)
 			end
 		end
 
-		return getsynasset(audioName) or "rbxasset://sounds/uuhhh.mp3"
+		return getsynasset("Timeless/"..audioName) or "rbxasset://sounds/uuhhh.mp3"
 	end
 end
 
 
 function Asset.write(url, assetName)
 	if works then
-		if not isfile(assetName) then
+		if not isfile("Timeless/"..assetName) then
 			print("attempt to download", assetName)
 			local Response, TempFile = request({Url = url, Method = 'GET'})
 			if Response.StatusCode == 200 then
-				writefile(assetName, Response.Body)
+				writefile("Timeless/"..assetName, Response.Body)
 				print("successful:", assetName, "-", getsynasset(assetName))
 			end
 		else
@@ -75,8 +78,8 @@ end
 
 function Asset.del(assetName)
 	if works then
-		if isfile(assetName) then
-			delfile(assetName)
+		if isfile("Timeless/"..assetName) then
+			delfile("Timeless/"..assetName)
 		else
 			warn(assetName, "is not a valid member of workspace!")
 		end
@@ -85,8 +88,8 @@ end
 
 function Asset.get(asset)
 	if works then
-		if isfile(asset) then
-			return getsynasset(asset)
+		if isfile("Timeless/"..asset) then
+			return getsynasset("Timeless/"..asset)
 		else
 			warn(asset, "is not a valid member of workspace!")
 			return
