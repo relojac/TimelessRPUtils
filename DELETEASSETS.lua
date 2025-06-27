51a7f10d5ff50682521f411e80d7226e62fb8209
relojac <121554584+relojac@users.noreply.github.com>
2025-06-27 13:25:47 +0800

    Update DELETEASSETS.lua

--- DELETEASSETS.lua
+++ DELETEASSETS.lua
@@ -1,18 +1,17 @@
 local genv = (getgenv and getgenv()) or shared
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/relojac/TimelessRPUtils/refs/heads/main/Core/Module/Asset.lua"))()
-
 genv.Module = genv.Module or {}
 local Module = genv.Module
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/relojac/TimelessRPUtils/refs/heads/main/Core/Module/Asset.lua"))()
 local Asset = Module.Asset
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/relojac/TimelessRPUtils/refs/heads/main/Assets/AssetTable.lua"))()
 local AssetTable = genv.AssetTable
 
 for name, entry in pairs(AssetTable) do
-	print("access", entry, "of AssetTable")
+	print("access", name, "of AssetTable")
 	for _, v in ipairs(entry) do
-		Asset.del(v, nil)
+		Asset.del("Timeless/" .. v)
 	end
 end
