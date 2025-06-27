75cb57fffd3e4215e27eaf4332cf0f834dfbeefe
relojac <121554584+relojac@users.noreply.github.com>
2025-06-27 17:34:13 +0800

    Update Main.lua

--- Core/Main.lua
+++ Core/Main.lua
@@ -53,7 +53,8 @@ loadstring(game:HttpGet("https://github.com/relojac/TimelessRPUtils/raw/refs/hea
 
 --|| BUTTONS ||--
 
-if Settings.Buttons.Sit.Enabled then loadstring(game:HttpGet("https://raw.githubusercontent.com/relojac/TimelessRPUtils/refs/heads/main/Core/Buttons/Sit.lua"))() end
 if Settings.Buttons.Faint.Enabled then loadstring(game:HttpGet("https://raw.githubusercontent.com/relojac/TimelessRPUtils/refs/heads/main/Core/Buttons/Faint.lua"))() end
+if Settings.Buttons.Reset.Enabled then loadstring(game:HttpGet("https://raw.githubusercontent.com/relojac/TimelessRPUtils/refs/heads/main/Core/Buttons/Reset.lua"))() end
+if Settings.Buttons.Sit.Enabled then loadstring(game:HttpGet("https://raw.githubusercontent.com/relojac/TimelessRPUtils/refs/heads/main/Core/Buttons/Sit.lua"))() end
 
 --|| ADDONS ||--
