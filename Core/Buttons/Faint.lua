63587df513fe374047f9442e014353e2550cc461
relojac <121554584+relojac@users.noreply.github.com>
2025-06-27 08:45:35 +0800

    Create Faint.lua

---
+++ Core/Buttons/Faint.lua
@@ -0,0 +1,46 @@
+local genv = (getgenv and getgenv()) or shared
+
+local Settings = genv.Settings
+local Module = genv.Module
+
+local Button = Module.Button
+local Positioner = Module.Positioner
+local Asset = Module.Asset
+
+
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+local Player = Players.LocalPlayer
+local Character = Player.Character or Player.CharacterAdded:Wait()
+local Humanoid = Character:WaitForChild("Humanoid")
+
+Player.CharacterAdded:Connect(function(new)
+	Character = new
+	Humanoid = Character:WaitForChild("Humanoid")
+end)
+
+local btn = "FaintButton"
+
+local off = Asset.get(btn .. "_off.png")
+local on = Asset.get(btn .. "_on.png")
+
+local button = Button.new(btn, true)
+	button.PressedImage = ""
+	button.Position = Positioner.get(Settings.Buttons.Faint.Position)
+
+button.MouseButton1Click:Connect(function()
+	if not Humanoid.PlatformStand then
+		Humanoid.PlatformStand = true
+	else
+		Humanoid.PlatformStand = false
+	end
+end)
+
+RunService.Stepped:Connect(function()
+	if not Humanoid.PlatformStand then
+		button.Image = off
+	else
+		button.Image = on
+	end
+end)
