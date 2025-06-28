02023d66692ae2a17ec9f98801ade418f32ce7e2
relojac <121554584+relojac@users.noreply.github.com>
2025-06-28 14:33:49 +0800

    Create nothingiswatching.lua

---
+++ Addons/nothingiswatching.lua
@@ -0,0 +1,61 @@
+local genv = (getgenv and getgenv()) or shared
+
+local Settings = genv.Settings.Addons.nothingiswatching
+local Module = genv.Module
+
+local Asset = Module.Asset
+
+
+local CRTEffect = Settings.CRTEffect
+local Shoving = Settings.S
+
+local Shove = game:HttpGet("https://github.com/relojac/TimelessRPUtils/raw/refs/heads/main/Core/RandomShit/Shove.lua")
+
+
+local Debris = game:GetService("Debris")
+local Players = game:GetService("Players")
+
+local Player = Players.LocalPlayer
+local PlayerGui = Player:WaitForChild("PlayerGui")
+
+--|| CRT EFFECT ||--
+
+local CRTimg = Asset.get("Timeless/crt.png")
+
+local CRTGui = Instance.new("ScreenGui", PlayerGui); do
+	CRTGui.Name = "CRTGui"
+	CRTGui.ResetOnSpawn = false
+	CRTGui.IgnoreGuiInset = true
+	CRTGui.Enabled = CRTEffect
+end
+
+local Reference = Instance.new("Frame", CRTGui); do
+	Reference.Name = "Reference"
+	Reference.Active = false
+	Reference.Position = UDim2.fromScale(0.5, 0)
+	Reference.Size = UDim2.fromScale(1, 1)
+	Reference.BackgroundTransparency = 1
+	Reference.ZIndex = -2147483648
+end
+
+local UIARC = Instance.new("UIAspectRatioConstraint", Reference); do
+	UIARC.AspectRatio = 4/3
+	UIARC.DominantAxis = Enum.DominantAxis.Width
+end
+
+local border = Instance.new("Frame"); do
+	border.Name = "Border"
+	border.Active = false
+	border.Size = UDim2.fromScale(1, 1)
+	border.BorderSizePixel = 0
+	border.BackgroundColor3 = Color3.new(0, 0, 0)
+end
+
+local left = border:Clone(); do
+	left.Parent = Reference
+	left.Position = UDim2.fromScale(-1, 0)
+end
+local right = border:Clone(); do
+	right.Parent = Reference
+	right.Position = UDim2.fromScale(-1, 0)
+end
