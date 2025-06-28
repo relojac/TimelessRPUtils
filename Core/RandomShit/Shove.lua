824e6e705e9c48e849d8b85a129a41ae4a6b33dd
relojac <121554584+relojac@users.noreply.github.com>
2025-06-28 13:56:30 +0800

    Update Shove.lua

--- Core/RandomShit/Shove.lua
+++ Core/RandomShit/Shove.lua
@@ -5,16 +5,17 @@ local Player = Players.LocalPlayer
 local Character = Player.Character
 local HRP = Character:WaitForChild("HumanoidRootPart")
 
-local X = math.random(-50, 50)
-local Y = math.random(20, 60)
-local Z = math.random(-50, 50)
+local X = math.random(-100, 100)
+local Y = math.random(50, 100)
+local Z = math.random(-100, 100)
 
-local Att = HRP:WaitForChild("RootRigAttachment")
+local Att = HRP:WaitForChild("RootAttachment")
 local LV = Instance.new("LinearVelocity"); do
 	LV.Name = "FlingForce"
 	LV.MaxForce = math.huge
 	LV.VectorVelocity = Vector3.new(X, Y, Z)
 	LV.Attachment0 = Att
+	LV.Parent = HRP
 end
 
 Debris:AddItem(LV, 0.1)
