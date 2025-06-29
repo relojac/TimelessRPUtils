1af4c9f9a81222982f8cee3fbccd165e1a6fd010
relojac <121554584+relojac@users.noreply.github.com>
2025-06-29 09:29:28 +0800

    Update Shove.lua

--- Core/RandomShit/Shove.lua
+++ Core/RandomShit/Shove.lua
@@ -5,17 +5,32 @@ local Player = Players.LocalPlayer
 local Character = Player.Character
 local HRP = Character:WaitForChild("HumanoidRootPart")
 
-local X = math.random(-30, 30)
-local Y = math.random(5, 20)
-local Z = math.random(-30, 30)
+local function shove()
+	local X = math.random(-30, 30)
+	local Y = math.random(5, 20)
+	local Z = math.random(-30, 30)
 
-local Att = HRP:WaitForChild("RootAttachment")
-local LV = Instance.new("LinearVelocity"); do
-	LV.Name = "FlingForce"
-	LV.MaxForce = math.huge
-	LV.VectorVelocity = Vector3.new(X, Y, Z)
-	LV.Attachment0 = Att
-	LV.Parent = HRP
+	local Att = HRP:WaitForChild("RootAttachment")
+	local LV = Instance.new("LinearVelocity"); do
+		LV.Name = "FlingForce"
+		LV.MaxForce = math.huge
+		LV.VectorVelocity = Vector3.new(X, Y, Z)
+		LV.Attachment0 = Att
+		LV.Parent = HRP
+	end
+
+	Debris:AddItem(LV, 0.1)
 end
 
-Debris:AddItem(LV, 0.1)
+task.spawn(function()
+	shove()
+	task.wait(math.random(0.5, 0.75)
+
+	shove()
+	task.wait(math.random(0.5, 0.75)
+
+	shove()
+	task.wait(math.random(0.5, 0.75)
+
+	shove()
+end)
