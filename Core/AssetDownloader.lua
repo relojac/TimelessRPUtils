fe9d40aa801848703631cb35b4fb700f38f05a65
relojac <121554584+relojac@users.noreply.github.com>
2025-06-30 14:42:09 +0800

    Update AssetDownloader.lua

--- Core/AssetDownloader.lua
+++ Core/AssetDownloader.lua
@@ -13,13 +13,20 @@ local Asset = Module.Asset
 loadstring(game:HttpGet("https://raw.githubusercontent.com/relojac/TimelessRPUtils/refs/heads/main/Assets/AssetTable.lua"))()
 local AssetTable = genv.AssetTable
 
+local activeTasks = 0
+
 for name, entry in pairs(AssetTable) do
 	print("access", name, "of AssetTable")
 	for _, v in ipairs(entry) do
 		task.spawn(function()
+			activeTasks += 1
 			if AlwaysRedownloadAssets then Asset.del("Timeless/" .. v) end
 		
 			Asset.write("https://github.com/relojac/TimelessRPUtils/raw/refs/heads/main/Assets/"..name.."/"..v.."/", "Timeless/" .. v)
+
+			activeTasks -= 1
 		end) 
 	end
 end
+
+repeat task.wait() until activeTasks == 0
