ce9f13ce10e484b16b44e965b6a8d41f3cc5fff9
relojac <relojac00057@gmail.com>
2025-07-23 23:32:40 +0800

    Update LimbsFirstPerson.lua

--- Addons/LimbsFirstPerson.lua
+++ Addons/LimbsFirstPerson.lua
@@ -1,10 +1,36 @@
 local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
 
 local Player = Players.LocalPlayer
 
+local RS, Died
+local currentState, lastState
 local function transparencyFix(char)
+	RS = RunService.RenderStepped:Connect(function()
+		local Humanoid = char:FindFirstChild("Humanoid")
+		local Head = char:FindFirstChild("Head")
+
+		currentState = Head.LocalTransparencyModifier <= 0 and "first" or "third"
+
+		if currentState ~= lastState then
+			if Head and Humanoid and currentState == "first" then
+				Humanoid.CameraOffset += Vector3.new(0, 0, 0.5)
+			else
+				Humanoid.CameraOffset -= Vector3.new(0, 0, 0.5)
+			end
+			
+			lastState = currentState
+		end
+	end
+
+	Died = char:WaitForChild("Humanoid").Died:Connect(function()
+		RS:Disconnect()
+		Died:Disconnect()
+	end)
+
+	
 	for _, v in char:GetChildren() do
-		if not v:IsA("BasePart") or v.Name == "Torso" or v.Name == "UpperTorso" or v.Name == "LowerTorso" then
+		if not v:IsA("BasePart") then
 			continue
 		end
 
@@ -16,7 +42,7 @@ local function transparencyFix(char)
 	end
 
 	char.ChildAdded:Connect(function(v)
-		if not v:IsA("BasePart") or v.Name == "Torso" or v.Name == "UpperTorso" or v.Name == "LowerTorso" then
+		if not v:IsA("BasePart") then
 			return
 		end
 
